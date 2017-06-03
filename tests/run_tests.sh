32dc7b9faa21ceba4fde718ff427e1980501ccd6
Sridhar Sarnobat <ss401533@gmail.com>
2017-06-02 20:54:31 -0700

    Minor changes (untested)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,8 @@
 cd ..
 javac ReportBotsExceedingEstimates.java
  
-java ReportBotsExceedingEstimates tests/emptyFile_input.txt 	| tee tests/emptyFile_output.txt
-java ReportBotsExceedingEstimates tests/emptyLines_input.txt	| tee tests/emptyLines_output.txt
-java ReportBotsExceedingEstimates tests/headings_input.txt		| tee tests/headings_output.txt
-java ReportBotsExceedingEstimates tests/invalidLines_input.txt	| tee tests/invalidLines_output.txt
+java ReportBotsExceedingEstimates tests/emptyFile_input.txt 		| tee tests/emptyFile_output.txt
+java ReportBotsExceedingEstimates tests/emptyLines_input.txt		| tee tests/emptyLines_output.txt
+java ReportBotsExceedingEstimates tests/headings_input.txt			| tee tests/headings_output.txt
+java ReportBotsExceedingEstimates tests/invalidLines_input.txt		| tee tests/invalidLines_output.txt
+java ReportBotsExceedingEstimates tests/whitespaceExcess_input.txt	| tee tests/whitespaceExcess_output.txt
